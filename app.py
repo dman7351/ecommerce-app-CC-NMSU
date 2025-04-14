19da992bee719b9f7ec78eb022d53257b4ed4013
dman7351 <dcarr7351@gmail.com>
2025-04-14 09:42:30 -0600

    removed main (not ran in gunicorn)

--- app.py
+++ app.py
@@ -174,8 +174,3 @@ def delete_data():
 
 	return render_template('delete_data.html', data=data, headers=headers, table=table)
 
-if __name__ == '__main__':
-	with app.app_context():
-		db.create_all()
-	app.run(host='0.0.0.0', port=5000, debug=True)
-
